4315731109b030b813c3e69c0563aa9d8735182a
RifqyYR <yudhiestrarifqy@gmail.com>
2023-11-23 09:07:28 +0800

    add logo

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="icon" type="image/png" sizes="32x32" href="http://wil4.ddns.net/img/favicon.png">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
     <title>SIAP</title>
